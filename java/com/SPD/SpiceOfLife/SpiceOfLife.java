00cc8eb3f3c6a64137a67551ac90ede24d7f61d4
bertie <bertie@bertie.io>
2015-10-17 23:50:30 +0100

    Added Cinnamon Bark drops from logs and Cinnamon Powder recipe

--- java/com/SPD/SpiceOfLife/SpiceOfLife.java
+++ java/com/SPD/SpiceOfLife/SpiceOfLife.java
@@ -3,11 +3,15 @@ package com.SPD.SpiceOfLife;
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.world.BlockEvent.HarvestDropsEvent;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
  
 @Mod(modid = SpiceOfLife.MODID, version = SpiceOfLife.VERSION)
@@ -17,7 +21,7 @@ public class SpiceOfLife
 	public static final String MODID = "SpiceOfLife";
     public static final String VERSION = "0.01";
     public static final String NAME = "Spice Of Life";
-    public static final String VERSIONNAME = "\"Anyone Here?\"";
+    public static final String VERSIONNAME = "\"Angry Armadillo\"";
     
     //Blocks!
     public static Block cinnamonWood;
@@ -30,12 +34,16 @@ public class SpiceOfLife
     
     @EventHandler
     public void preinit(FMLPreInitializationEvent event){
+    //Events!
+        MinecraftForge.EVENT_BUS.register(new SpiceOfLifeEvents());
+
+    	
     //Messages!
     	System.out.println("Started Loading " + SpiceOfLife.NAME + " " + SpiceOfLife.VERSION + " " + SpiceOfLife.VERSIONNAME);
     
     //Blocks!	
     	cinnamonWood = new blockCinnamonWood();
-    	GameRegistry.registerBlock(cinnamonWood, "cinnamonWood");
+    //	GameRegistry.registerBlock(cinnamonWood, "cinnamonWood");
    
     //Items!
     	cinnamonBark = new itemCinnamonBark();
@@ -44,16 +52,27 @@ public class SpiceOfLife
     	cinnamonPowder = new itemCinnamonPowder();
     	GameRegistry.registerItem(cinnamonPowder, "cinnamonPowder");
     
+    //Recipes
+    	GameRegistry.addShapelessRecipe(new ItemStack(SpiceOfLife.cinnamonPowder), new Object[]
+    			{
+    			SpiceOfLife.cinnamonBark
+    			
+    			});
     }
     
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
- 
+
     }
     @EventHandler
     public void postInit(FMLPostInitializationEvent event)
     {
     	System.out.println("Finished Loading " + SpiceOfLife.NAME + " " + SpiceOfLife.VERSION + " " + SpiceOfLife.VERSIONNAME);
     }
+    
+    //Drops!
+    
+   
+
 }
---
+++ java/com/SPD/SpiceOfLife/SpiceOfLifeEvents.java
@@ -0,0 +1,16 @@
+package com.SPD.SpiceOfLife;
+
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.event.world.BlockEvent.HarvestDropsEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+
+public class SpiceOfLifeEvents {
+	  @SubscribeEvent
+	    public void onBlockHarvest(HarvestDropsEvent event){
+	    
+		//CinnamonBark  
+		  if (event.block == Blocks.log)
+	    		event.drops.add(new ItemStack(SpiceOfLife.cinnamonBark));
+	    }
+}
